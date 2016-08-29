65d385cf17a85d1c6729a96477f7e240f61ff170
ramandeep <ramandeep738@gmail.com>
2016-08-29 13:08:24 +0530

    NUnit-TDD

---
+++ GetOrder1/MathTestProjects/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathComponentsNameSpace;
+namespace MathTestProjects
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            MathComponents obj = new MathComponents();
+            int result = obj.Add(10, 10);
+            Assert.AreEqual(20, result);
+
+        }
+
+        [TestMethod]
+        public void TestSubstract()
+        {
+            MathComponents obj = new MathComponents();
+            int result = obj.Substract(60, 10);
+            Assert.AreEqual(50, result);
+        }
+    }
+}
