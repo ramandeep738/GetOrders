7b6a8a3ab7689b81e1f7d9d64bb01ac3b8a50b4f
ramandeep <ramandeep738@gmail.com>
2016-08-29 13:08:02 +0530

    MathComponents

---
+++ GetOrder1/MathComponents/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathComponentsNameSpace
+{
+    public class MathComponents
+    {
+        public int Add(int Num1,int Num2)
+        {
+            return Num1 + Num2;
+        }
+
+        public int Substract(int Num1, int Num2)
+        {
+            return Num1 - Num2;
+        }
+        
+    }
+}
